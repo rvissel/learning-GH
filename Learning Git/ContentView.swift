1c5fc82714612c9918396edb759039b645dd2c53
Roi Vissel <roivissel@gmail.com>
2022-05-17 12:02:03 -0700

    Initial Commit

---
+++ Learning Git/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning Git
+//
+//  Created by Roi on 5/17/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning Git/Learning_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_GitApp.swift
+//  Learning Git
+//
+//  Created by Roi on 5/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
